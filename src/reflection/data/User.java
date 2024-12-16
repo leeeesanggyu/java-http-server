885fc8ce400861e22cb3074ff0884cb8134367b7
이상규 <76906458+leeeesanggyu@users.noreply.github.com>
2024-12-16 16:07:38 +0900

    Reflection 활용 예제 (null field 치환)

--- src/reflection/data/User.java
+++ src/reflection/data/User.java
@@ -4,9 +4,9 @@ public class User {
 
     private String id;
     private String name;
-    private int age;
+    private Integer age;
 
-    public User(String id, String name, int age) {
+    public User(String id, String name, Integer age) {
         this.id = id;
         this.name = name;
         this.age = age;
